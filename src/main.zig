1808313f2a0b320dd9c1aca68cb6d4996d5e10c5
Krist Ponpairin <krist7599555@gmail.com>
2026-02-11 17:26:20 +0700

    feat: add bounding system

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,8 @@ const Player = struct {
     forward: Vec3, // force normalize && y = 0
     velocity: Vec3, // force x = 0, z = 0
 
+    const radius: f32 = 0.5;
+    const height: f32 = 1.0;
     const Y = Vec3.init(0.0, 1.0, 0.0);
     const moveSpeed: f32 = 5.0;
     const turnSpeed: f32 = 3.0;
@@ -18,7 +20,14 @@ const Player = struct {
         return std.math.atan2(self.forward.x, self.forward.z);
     }
 
-    fn update(self: *Player, dt: f32) void {
+    fn getBounds(self: Player) rl.BoundingBox {
+        return .{
+            .min = .{ .x = self.position.x - radius, .y = self.position.y, .z = self.position.z - radius },
+            .max = .{ .x = self.position.x + radius, .y = self.position.y + height, .z = self.position.z + radius },
+        };
+    }
+
+    fn update(self: *Player, dt: f32, ground_box: rl.BoundingBox) void {
         // --- Rotation ---
         var angle_: f32 = 0;
         if (rl.isKeyDown(.a)) angle_ += turnSpeed * dt;
@@ -32,15 +41,21 @@ const Player = struct {
         if (rl.isKeyDown(.s)) walk_ -= moveSpeed * dt;
         self.position = self.position.add(self.forward.scale(walk_));
 
-        // --- JUMP & GRAVITY ---
-        self.velocity =
-            if (self.position.y == 0.0 and rl.isKeyPressed(.space))
-                jumpForce
-            else
-                self.velocity.add(gravity.scale(dt));
-
+        // --- PHYSICS: Gravity ---
+        self.velocity = self.velocity.add(gravity.scale(dt));
         self.position = self.position.add(self.velocity.scale(dt));
-        if (self.position.y < 0.0) self.position.y = 0.0;
+
+        // --- Collision Checker System ---
+        const playerBounds = self.getBounds();
+        if (rl.checkCollisionBoxes(playerBounds, ground_box)) {
+            // ถ้าชนพื้น ให้หยุดตกลงมา และวางเท้าบนพื้นพอดี
+            self.position.y = ground_box.max.y;
+            self.velocity.y = 0;
+
+            if (rl.isKeyPressed(.space)) {
+                self.velocity = self.velocity.add(jumpForce);
+            }
+        }
     }
 
     fn draw(self: Player, shader: rl.Shader) void {
@@ -53,13 +68,25 @@ const Player = struct {
         rl.gl.rlTranslatef(self.position.x, self.position.y, self.position.z);
         rl.gl.rlRotatef(self.angle() * 180.0 / std.math.pi, 0.0, 1.0, 0.0);
 
-        rl.drawCylinder(Vec3.zero(), 0.5, 0.7, 1.0, 12, .white);
-        rl.drawSphere(Vec3.init(0.0, 1.0, 0.5), 0.1, .white);
-        rl.drawCylinderWires(Vec3.zero(), 0.5, 0.7, 1.0, 12, .white);
+        const centerOffset = Vec3.init(0.0, height / 2.0, 0.0);
+        rl.drawCylinder(centerOffset, radius, radius * 1.2, height, 12, .white);
+        rl.drawSphere(Vec3.init(0.0, height, 0.5 * radius), 0.1, .white);
+        rl.drawCylinderWires(centerOffset, radius, radius * 1.2, height, 12, .white);
     }
 };
 
 const Ground = struct {
+    bounds: rl.BoundingBox,
+
+    fn init() Ground {
+        return .{
+            .bounds = .{
+                .min = .{ .x = -10, .y = -0.1, .z = -10 },
+                .max = .{ .x = 10, .y = 0.0, .z = 10 },
+            },
+        };
+    }
+
     fn draw(_: Ground) void {
         rl.drawPlane(Vec3.init(0.0, 0.0, 0.0), Vec2.init(20, 20), .green);
         rl.drawGrid(20, 1.0);
@@ -81,7 +108,7 @@ pub fn main() anyerror!void {
         .velocity = Vec3.zero(),
         // .angle = 0,
     };
-    const ground = Ground{};
+    const ground = Ground.init();
 
     // ตั้งค่ากล้อง 3D
     var camera = rl.Camera3D{
@@ -99,7 +126,7 @@ pub fn main() anyerror!void {
         const dt = rl.getFrameTime();
 
         // UPDATE Logic
-        player.update(dt);
+        player.update(dt, ground.bounds);
 
         // UPDATE Camera (Third Person)
         {
@@ -107,7 +134,7 @@ pub fn main() anyerror!void {
             const camHeight: f32 = 2.5;
             camera.position = player.position.subtract(player.forward.scale(camDistance));
             camera.position.y += camHeight;
-            camera.target = player.position;
+            camera.target = player.position.add(Vec3.init(0.0, 0.5, 0.0)); // มองไปที่กลางตัว
         }
 
         // DRAW Logic
