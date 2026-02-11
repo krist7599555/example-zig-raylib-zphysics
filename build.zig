9bae847d1206bfefddc60f5c3c18f7d7c7bb2aa0
Krist Ponpairin <krist7599555@gmail.com>
2026-02-11 14:43:08 +0700

    first window

--- build.zig
+++ build.zig
@@ -58,6 +58,15 @@ pub fn build(b: *std.Build) void {
     //
     // If neither case applies to you, feel free to delete the declaration you
     // don't need and to put everything under a single module.
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+
     const exe = b.addExecutable(.{
         .name = "zig_car_gl",
         .root_module = b.createModule(.{
@@ -83,6 +92,10 @@ pub fn build(b: *std.Build) void {
             },
         }),
     });
+
+    exe.root_module.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
 
     // This declares intent for the executable to be installed into the
     // install prefix when running `zig build` (i.e. when executing the default
